003820e61b2a3179379acf23e0f708fedd295120
Pajjaphon Whanchid <guyguy5050@gmail.com>
2023-03-11 00:20:45 +0700

    Create Event List, Event Item Component

---
+++ components/events/event-list.js
@@ -0,0 +1,28 @@
+import EventItem from "./event-item";
+
+import classes from "../../styles/event-list.module.css";
+
+const EventList = (props) => {
+  console.log(props);
+
+  const { items } = props;
+
+  return (
+    <ul className={classes.list}>
+      {items.map((item) => {
+        return (
+          <EventItem
+            key={item.id}
+            id={item.event}
+            title={item.title}
+            location={item.location}
+            date={item.date}
+            image={item.image}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+export default EventList;
